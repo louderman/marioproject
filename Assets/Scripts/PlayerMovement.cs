856a20ab85559702cce85b970e015b68b3aa1a51
louderman <danielmirza02@gmail.com>
2026-09-23 22:18:12 +0800

    mario horizontal movement using legacy input handling

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+// TODO: unity complained about me using legacy input handling. maybe recode this later down the line? priority: low
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 10;
+    public float maxSpeed = 20;
+    private Rigidbody2D marioBody;
+
+    // Start is called before the first frame update
     void Start()
     {
-        
+        // Set to be 30 FPS
+        Application.targetFrameRate =  30;
+        marioBody = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-}
+
+    // FixedUpdate may be called once per frame. See documentation for details.
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Mathf.Abs(moveHorizontal) > 0){
+            Vector2 movement = new Vector2(moveHorizontal, 0);
+            // check if it doesn't go beyond maxSpeed
+            if (marioBody.linearVelocity.magnitude < maxSpeed)
+                    marioBody.AddForce(movement * speed);
+        }
+
+        // stop
+        if (Input.GetKeyUp("a") || Input.GetKeyUp("d")){
+            // stop
+            marioBody.linearVelocity = Vector2.zero;
+        }
+  }
